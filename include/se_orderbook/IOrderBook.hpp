ef2c031e54f8721111202ffd3cb0f13306904353
Kushagra Srivastava <kushagra.srivastava@student.manchester.ac.uk>
2025-04-14 00:04:30 +0200

    feat: add order book interface and normalized order update header

---
+++ include/se_orderbook/IOrderBook.hpp
@@ -0,0 +1,19 @@
+#ifndef SE_ORDERBOOK_IORDERBOOK_HPP
+#define SE_ORDERBOOK_IORDERBOOK_HPP
+
+#include <vector>
+#include <string>
+#include <Side.hpp>
+#include <OrderEntry.hpp>
+
+struct NormalizedOrderUpdate;
+
+class IOrderBook {
+public:
+    virtual void apply_update(const NormalizedOrderUpdate& update) = 0;
+    virtual std::vector<OrderEntry> get_top_n(Side side, int n) const = 0;
+    virtual std::string get_exchange_name() const = 0;
+    virtual ~IOrderBook() = default;    
+};
+
+#endif
