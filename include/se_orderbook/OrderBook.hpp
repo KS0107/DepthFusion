2ebade19396fe22d9519d44328cc76706b04bc33
Kushagra Srivastava <kushagra.srivastava@student.manchester.ac.uk>
2025-04-14 00:09:46 +0200

    feat: add orderbook header

---
+++ include/se_orderbook/OrderBook.hpp
@@ -0,0 +1,24 @@
+#ifndef SE_ORDERBOOK_ORDERBOOK_HPP
+#define SE_ORDERBOOK_ORDERBOOK_HPP
+
+#include <string>
+#include <IOrderBook.hpp>
+#include <OrderBookSide.hpp>
+#include <NormalizedOrderUpdate.hpp>
+
+class OrderBook : public IOrderBook {
+public:
+    OrderBook(std::string exchange_name, std::string symbol);
+
+    void apply_update(const NormalizedOrderUpdate& update) override;
+    std::vector<OrderEntry> get_top_n(Side side, int n) const override;
+    std::string get_exchange_name() const override;
+
+private:
+    std::string exchange_name_;
+    std::string symbol_;
+    OrderBookSide bids_;
+    OrderBookSide asks_;
+};
+
+#endif
